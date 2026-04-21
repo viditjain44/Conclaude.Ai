36de4fa3ccee9395e63d59f2479b87ec6480ba25
Vidit Jain <jainvidit365vidit@gmail.com>
2026-04-21 12:13:43 +0530

    cors and .env fixed

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: require('path').resolve(__dirname, '../.env') });
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
@@ -11,7 +11,10 @@ const insightsRoute = require("./routes/insights");
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
